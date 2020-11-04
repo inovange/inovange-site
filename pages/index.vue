af45c497c183e069e43cc250288f62b6dd49bef2
yusei66 <gai.face2001@icloud.com>
2020-11-04 19:14:24 +0900

    Revert "member page"
    This reverts commit 4a7d35aeb0cfc45806c0dc7ace8601fd974118b4.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,29 @@
 <template>
-  <div class="container main">
-    <div class="home tag">
-      Home
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        inovange-site
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
-
-    <div class="containert">
-      <h1>Member</h1>
-    </div>
-
-    <div class="container sub">
-
-      <div class="container1">
-
-      </div>
-      <div class="container2">
-
-      </div>
-      <div class="container3">
-
-      </div>
-      <div class="container4">
-
-      </div>
-
-    </div>
-
   </div>
 </template>
 
@@ -33,8 +32,42 @@ export default {}
 </script>
 
 <style>
-containert {
-  color: #3b8070;
-  border-color: #35495e;
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
 }
 </style>
