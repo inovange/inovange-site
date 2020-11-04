ecd4a6d0553baa2669aa7dc53302ca2a74e6aff0
yusei66 <gai.face2001@icloud.com>
2020-11-04 15:07:21 +0900

    member-pages text/image

---
+++ pages/Inovange-site-portforio.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container main">
+    <div class="home tag">
+      <span>Home</span>
+    </div>
+
+    <div class="containert">
+      <h1>作品集</h1>
+    </div>
+
+    <div class="container sub">
+
+      <div class="container1">
+        <img src="../static/jm-dav-dhAgfU99RCs-unsplash.jpg" height="200" width=""/>
+      </div>
+      <div class="container2">
+        <img src="../static/thomas-yohei-0fjRZf08HJQ-unsplash.jpg" height="300" width=""/>
+      </div>
+      <div class="container3">
+        <img src="../static/ben-klewais-APi5Pa4bEcI-unsplash.jpg" height="300" width=""/>
+      </div>
+      <div class="container4">
+        <img src="../static/michael-bowman-GVMdjXsFpXo-unsplash.jpg" height="200" width=""/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+span {
+  border: 1px solid;
+  border-color: #35495e;
+}
+
+h1 {
+}
+
+.containert {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  font-size: 50px;
+}
+
+.container1 {
+  margin-top: 100px;
+  border: 2px solid black;
+  width: auto;
+  height: 400px;
+}
+
+.container2 {
+  margin-top: 100px;
+  border: 2px solid black;
+  width: auto;
+  height: 400px;
+}
+
+.container3 {
+  margin-top: 100px;
+  border: 2px solid black;
+  width: auto;
+  height: 400px;
+}
+
+.container4 {
+  margin-top: 100px;
+  border: 2px solid black;
+  width: auto;
+  height: 400px;
+}
+</style>
