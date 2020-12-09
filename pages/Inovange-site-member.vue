77b27c07ec43eb5a2d5a60f347c32ca049237ac2
hiroto940 <hiroto091488@gmail.com>
2020-12-09 18:36:51 +0900

    inovange-site-yusei pull request

---
+++ pages/Inovange-site-member.vue
@@ -0,0 +1,319 @@
+<template>
+  <div class="container main">
+    <div class="home tag">
+      <span>Home</span>
+    </div>
+    <div class="animation">
+      <svg id="レイヤー_1" data-name="レイヤー 1" class="abc" xmlns="http://www.w3.org/2000/svg">
+        <path class="cls-1 svg-elem-1" d="M466.82,394.74C654.05,593.16,841.68,631.63,1030,396.09" />
+        <path class="cls-2 svg-elem-2" d="M102.62,392.39c103.29-186.74,225.8-135.29,355.2,1.35" />
+        <line class="cls-3 svg-elem-3" x1="-134" y1="394" x2="1309" y2="394" />
+        <text class="cls-4" transform="translate(457.57 375.81)">Member</text>
+      </svg>
+    </div>
+    <div class="container1">
+      <div class="container-i-1">
+        <img src="../static/IMG_8981.JPG" height="" width="100%">
+      </div>
+      <div class="container-t-1">
+        <h2>心が”ゆれる”ものを創る</h2>
+        <p>名前：豊永 雄正</p>
+        <p>信念：心がゆれるものを創る</p>
+        <table>
+          <tr>
+            <th>2016年3月</th>
+            <td>丸岡南中学校卒業</td>
+          </tr>
+          <tr>
+            <th>2020年3月</th>
+            <td>北陸高等学校卒業</td>
+          </tr>
+          <tr>
+            <th>2020年4月</th>
+            <td>富山大学入学</td>
+          </tr>
+        </table>
+        <p>
+          思い：僕にとって、”ゆれる”は”つながる”であり、”革新”の意味でもあります。これらを僕やその仲間と関わった人達に提供させていただきたい！そんな思いでこのチームを結成致しました。まずは、広告やwebページを通してお客様とそのお客様を”つないで”いきます。
+        </p>
+      </div>
+    </div>
+    <div class="container2">
+      <div class="container-i-2">
+        <img src="../static/IMG_8946.JPG" height="200px" width="">
+      </div>
+      <div class="container-t-2">
+        <p>アイウエオ</p>
+      </div>
+    </div>
+    <div class="container3">
+      <div class="container-i-3">
+        <img src="../static/IMG_8981.JPG" height="200px" width="">
+      </div>
+      <div class="container-t-3">
+        <p>アイウエオ</p>
+      </div>
+    </div>
+    <div class="container4">
+      <div class="container-i-4">
+        <img src="../static/IMG_8981.JPG" height="200px" width="">
+      </div>
+      <div class="container-t-4">
+        <p>アイウエオ</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.cls-1 {
+  fill: #9a0010;
+}
+
+.cls-1, .cls-2, .cls-3 {
+  stroke: #9a0010;
+  stroke-miterlimit: 10;
+}
+
+.cls-1, .cls-2 {
+  stroke-width: 1.51px;
+}
+
+.cls-2 {
+  fill: #040000;
+}
+
+.cls-3 {
+  fill: none;
+  stroke-width: 2.24px;
+}
+
+.cls-4 {
+  font-size: 66.4px;
+  fill: #231815;
+  font-family: KozGoPr6N-Regular-83pv-RKSJ-H, Kozuka Gothic Pr6N;
+  letter-spacing: 0.18em;
+}
+span {
+  border: 1px solid;
+  border-color: #35495e;
+}
+
+.container-i-1 {
+  width: 50%;
+  float: left;
+}
+
+.container-t-1 {
+  width: 50%;
+  float: left;
+}
+
+.container1 {
+  margin: auto;
+  margin-bottom: 140px;
+  border: 2px solid black;
+  width: 100%;
+  height: 400px;
+}
+
+.container-i-2 {
+  width: 50%;
+  float: left;
+}
+
+.container-t-2 {
+  width: 50%;
+  float: left;
+}
+
+.container2 {
+  margin: auto;
+  margin-bottom: 140px;
+  border: 2px solid black;
+  width: 100%;
+  height: 400px;
+}
+
+.container-i-3 {
+  width: 50%;
+  float: left;
+}
+
+.container-t-3 {
+  width: 50%;
+  float: left;
+}
+
+.container3 {
+  margin: auto;
+  margin-bottom: 140px;
+  border: 2px solid black;
+  width: 100%;
+  height: 400px;
+}
+
+.container-i-4 {
+  width: 50%;
+  float: left;
+}
+
+.container-t-4 {
+  width: 50%;
+  float: left;
+}
+
+.container4 {
+  margin: auto;
+  margin-bottom: 140px;
+  border: 2px solid black;
+  width: 100%;
+  height: 400px;
+}
+
+.abc {
+  width: 100%;
+  height: 700px;
+  position: absolute;
+  top: 50%;
+  left: 45%;
+  transform: translate(-50%, -50%);
+}
+
+.animation {
+  width: 1200px;
+  height: 700px;
+  position: relative;
+}
+
+@-webkit-keyframes animate-svg-stroke-1 {
+  0% {
+    stroke-dashoffset: 673.2532348632812px;
+    stroke-dasharray: 673.2532348632812px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@keyframes animate-svg-stroke-1 {
+  0% {
+    stroke-dashoffset: 673.2532348632812px;
+    stroke-dasharray: 673.2532348632812px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@-webkit-keyframes animate-svg-fill-1 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(154, 0, 16);
+  }
+}
+
+@keyframes animate-svg-fill-1 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(154, 0, 16);
+  }
+}
+
+.svg-elem-1 {
+  -webkit-animation: animate-svg-stroke-1 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0s both,
+  animate-svg-fill-1 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 0.8s both;
+  animation: animate-svg-stroke-1 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0s both,
+  animate-svg-fill-1 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 0.8s both;
+}
+
+@-webkit-keyframes animate-svg-stroke-2 {
+  0% {
+    stroke-dashoffset: 449.749267578125px;
+    stroke-dasharray: 449.749267578125px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@keyframes animate-svg-stroke-2 {
+  0% {
+    stroke-dashoffset: 449.749267578125px;
+    stroke-dasharray: 449.749267578125px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@-webkit-keyframes animate-svg-fill-2 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(4, 0, 0);
+  }
+}
+
+@keyframes animate-svg-fill-2 {
+  0% {
+    fill: transparent;
+  }
+
+  100% {
+    fill: rgb(4, 0, 0);
+  }
+}
+
+.svg-elem-2 {
+  -webkit-animation: animate-svg-stroke-2 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0.12s both,
+  animate-svg-fill-2 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 0.9s both;
+  animation: animate-svg-stroke-2 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0.12s both,
+  animate-svg-fill-2 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 0.9s both;
+}
+
+@-webkit-keyframes animate-svg-stroke-3 {
+  0% {
+    stroke-dashoffset: 1445px;
+    stroke-dasharray: 1445px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+@keyframes animate-svg-stroke-3 {
+  0% {
+    stroke-dashoffset: 1445px;
+    stroke-dasharray: 1445px;
+  }
+
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+
+.svg-elem-3 {
+  -webkit-animation: animate-svg-stroke-3 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0.24s both,
+  animate-svg-fill-3 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 1s both;
+  animation: animate-svg-stroke-3 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0.24s both,
+  animate-svg-fill-3 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 1s both;
+}
+</style>
