dd285790da0e6caef702807415dd2f6b03bb5931
akihiro0710 <acedia2074@gmail.com>
2021-01-08 19:10:54 +0900

    リファクタ

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,18 @@
     <div class="mx-auto" style="width: 200px;">
       <canvas id="myChartc" class="my-4 w-100" width="900" height="400" />
     </div>
-    <div>
+    <div class="text-center">
       <Logo />
       <canvas id="myChartb" class="my-4 w-100" width="900" height="50" />
-      <center>
-        <circle-button link="/origin">What's Inovange</circle-button>
-        <circle-button link="/member">members</circle-button>
-        <circle-button link="/portforio">works</circle-button>
-      </center>
+      <circle-button link="/origin">
+        What's Inovange
+      </circle-button>
+      <circle-button link="/member">
+        members
+      </circle-button>
+      <circle-button link="/portforio">
+        works
+      </circle-button>
     </div>
   </div>
 </template>
